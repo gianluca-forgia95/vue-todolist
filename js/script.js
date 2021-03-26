de9208d4611bca0bcc053ea098cdcd22f2a11c14
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-26 14:18:26 +0100

    stampo gli item in pagina

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+/*
+- stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+*/
+
 var root = new Vue({
 el: '#root',
 data: {
+  list: [
+  'Chiamare amici',
+  'Fare il prepartita',
+  'Cantare fino al 90esimo',
+  'Parcheggiare vicino lo stadio',
+  ],
 
 
    },
    methods: {
-  
+
 
    }
 
